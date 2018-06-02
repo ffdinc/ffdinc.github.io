69385122d072d1e9f241c7cbfecaa59cbefc3ed0
BaherFarag <baher.tharwatt@gmail.com>
2018-06-02 14:29:23 +0200

    send grid updates

--- assets/js/bundle.js
+++ assets/js/bundle.js
@@ -40428,6 +40428,7 @@ WError.prototype.cause = function we_cause(c)
 };
 
 },{"assert-plus":73,"core-util-is":88,"extsprintf":95,"util":381}],197:[function(require,module,exports){
+(function (process){
 
 window.SendGridMail =function(mailfrom,mailto,mailsubject,mailhtml){
   SendMail(mailfrom,mailto,mailsubject,mailhtml)
@@ -40435,22 +40436,19 @@ window.SendGridMail =function(mailfrom,mailto,mailsubject,mailhtml){
 
 function SendMail(mailfrom,mailto,mailsubject,mailhtml)
 {
-
- var sendgridapiid = "3nZYDI9mSaGFctKSOltg0Q";
- var sendgridapikey = "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";
- 
 const sgMail = require('@sendgrid/mail');
-sgMail.setApiKey(sendgridapikey);
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 const msg = {
   to: mailto,
   from: mailfrom,
   subject: mailsubject,
   text: 'and easy to do anywhere, even with Node.js',
-  html: mailhtml
+  html: mailhtml,
 };
 sgMail.send(msg);
 }
-},{"@sendgrid/mail":23}],198:[function(require,module,exports){
+}).call(this,require('_process'))
+},{"@sendgrid/mail":23,"_process":332}],198:[function(require,module,exports){
 
 },{}],199:[function(require,module,exports){
 var asn1 = exports;
